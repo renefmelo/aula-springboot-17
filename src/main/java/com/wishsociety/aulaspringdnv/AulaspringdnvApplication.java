f5ac3b68d9a3938ccc30d47cd0df32e2691c13ef
Rene Melo <renevitorf@gmail.com>
2025-02-13 14:22:17 -0300

    dnv

---
+++ src/main/java/com/wishsociety/aulaspringdnv/AulaspringdnvApplication.java
@@ -0,0 +1,13 @@
+package com.wishsociety.aulaspringdnv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaspringdnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaspringdnvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wishsociety/aulaspringdnv/AulaspringdnvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishsociety.aulaspringdnv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaspringdnvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
